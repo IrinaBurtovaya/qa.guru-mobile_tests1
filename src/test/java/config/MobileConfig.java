a1a9de18c605a32196e2a3c2c0077260ba1f3781
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-06-19 14:06:47 +0300

    mobile test has been created

---
+++ src/test/java/config/MobileConfig.java
@@ -0,0 +1,31 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:config/mine.properties")
+public interface MobileConfig extends Config {
+
+    @Key("browserstack.user")
+    String browserstackUser();
+
+    @Key("browserstack.key")
+    String browserstackKey();
+
+    @Key("app")
+    String app();
+
+    @Key("device")
+    String device();
+
+    @Key("os_version")
+    String osVersion();
+
+    @Key("project")
+    String project();
+
+    @Key("build")
+    String build();
+
+    @Key("name")
+    String name();
+}
