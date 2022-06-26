dda68b95c4a55f261de4636738f50671043cdf5a
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-06-26 22:19:13 +0300

    hw 22 done

--- src/test/java/MobileTests.java
+++ src/test/java/MobileTests.java
@@ -1,5 +1,4 @@
 import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 
@@ -18,31 +17,5 @@ public class MobileTests extends TestBase {
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(CollectionCondition.containExactTextsCaseSensitive("Java (programming language)"))
         );
-    }
-
-    @Test
-    void onBoardingTests() {
-        step("StartPage", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(Condition.text("The Free Encyclopedia …in over 300 languages"));
-        });
-
-        step("SecondPage", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(Condition.text("New ways to explore"));
-        });
-
-        step("ThirdPage", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(Condition.text("Reading lists with sync"));
-        });
-
-        step("ForthPage", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(Condition.text("Send anonymous data"));
-        });
     }
 }
